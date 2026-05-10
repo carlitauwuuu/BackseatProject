331fbcc337386238429ca7bf14f7f5aee3b539ef
carlitauwuuu <mivicarlaa@gmail.com>
2026-05-10 16:18:20 +0200

    ya no se crean 29109019 carreteras ole olee

--- Assets/Scripts/SectionTrigger.cs
+++ Assets/Scripts/SectionTrigger.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
-using UnityEditor;
 
 public class SectionTrigger : MonoBehaviour
 {
-
     public GameObject roadSection;
+
+    private bool canSpawn = true;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Trigger"))
+        if (other.CompareTag("Trigger") && canSpawn)
         {
+            canSpawn = false;
+
             Instantiate(roadSection, new Vector3(-900, 28, -86), Quaternion.identity);
+
+            Invoke(nameof(ResetSpawn), 0.2f);
         }
     }
 
+    void ResetSpawn()
+    {
+        canSpawn = true;
+    }
 }
